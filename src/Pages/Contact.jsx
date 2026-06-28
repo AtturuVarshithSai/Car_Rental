db3aaea7b9ceda93ade6843ab20d598d3b269b8f
Varshith Sai <102579370+AtturuVarshithSai@users.noreply.github.com>
2026-06-28 17:45:01 +0530

    Update Contact.jsx

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -32,7 +32,7 @@ function Contact() {
                 <label>
                   Full Name <b>*</b>
                 </label>
-                <input type="text" placeholder='E.g: "Rahul Sharma"'></input>
+                <input type="text" placeholder='E.g: "Varshith Sai"'></input>
 
                 <label>
                   Email <b>*</b>
